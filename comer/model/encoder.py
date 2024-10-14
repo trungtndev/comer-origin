bd0e9cc50c0054e6443456359f286a965d39e53e
anhtienpro25 <trunglh113@gmail.com>
2024-10-14 20:51:30 +0700

    commit

--- comer/model/encoder.py
+++ comer/model/encoder.py
@@ -17,7 +17,19 @@ class ChannelAttention(nn.Module):
             nn.AdaptiveAvgPool2d(1),
             nn.AdaptiveMaxPool2d(1)
         ])
-        self.excitation = nn.Sequential(
+        self.excitation1 = nn.Sequential(
+            nn.BatchNorm2d(channels),
+            nn.Conv2d(in_channels=channels,
+                      out_channels=channels // reduction_rate,
+                      kernel_size=1),
+            nn.BatchNorm2d(channels // reduction_rate),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=channels // reduction_rate,
+                      out_channels=channels,
+                      kernel_size=1),
+            nn.BatchNorm2d(channels),
+        )
+        self.excitation2 = nn.Sequential(
             nn.BatchNorm2d(channels),
             nn.Conv2d(in_channels=channels,
                       out_channels=channels // reduction_rate,
@@ -36,8 +48,8 @@ class ChannelAttention(nn.Module):
         avg_feat = self.squeeze[0](x)
         max_feat = self.squeeze[1](x)
         # perform excitation with the same excitation sub-net
-        avg_out = self.excitation(avg_feat)
-        max_out = self.excitation(max_feat)
+        avg_out = self.excitation1(avg_feat)
+        max_out = self.excitation2(max_feat)
         # attention
         attention = self.sigmoid(avg_out + max_out)
         return attention * x
@@ -278,8 +290,8 @@ if __name__ == "__main__":
     for layer in encoder.children():
         layer.register_backward_hook(print_grad)
 
-    print("param:",sum(p.numel() for p in encoder.parameters() if p.requires_grad))
-    print(encoder)
+    # print("param:",sum(p.numel() for p in encoder.parameters() if p.requires_grad))
+    # print(encoder)
 
     print(encoder(img, img_mask)[0].shape)
     # print(encoder(img, img_mask)[1].shape)
