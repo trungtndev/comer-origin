cf71cde44aae2d97bccffcc05911536dadd0b082
anhtienpro25 <trunglh113@gmail.com>
2024-10-15 15:20:05 +0700

    commit

--- comer/model/encoder.py
+++ comer/model/encoder.py
@@ -10,75 +10,6 @@ from torch import FloatTensor, LongTensor
 
 from .pos_enc import ImgPosEnc
 
-class ChannelAttention(nn.Module):
-    def __init__(self, channels, reduction_rate=16):
-        super(ChannelAttention, self).__init__()
-
-        self.squeeze = nn.ModuleList([
-            nn.AdaptiveAvgPool2d(1),
-            nn.AdaptiveMaxPool2d(1)
-        ])
-
-        self.excitation = nn.Sequential(
-            nn.Conv2d(in_channels=channels,
-                      out_channels=channels // reduction_rate,
-                      kernel_size=1,
-                      bias=True),
-            nn.GELU(),
-            nn.Conv2d(in_channels=channels // reduction_rate,
-                      out_channels=channels,
-                      kernel_size=1,
-                      bias=True),
-        )
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        # perform squeeze with independent Pooling
-        avg_feat = self.squeeze[0](x)
-        max_feat = self.squeeze[1](x)
-        # perform excitation with the same excitation sub-net
-        avg_out = self.excitation(avg_feat)
-        max_out = self.excitation(max_feat)
-        # attention
-        attention = self.sigmoid(avg_out + max_out)
-        return attention * x
-
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
-
-        self.conv = nn.Conv2d(
-            in_channels=2,
-            out_channels=1,
-            kernel_size=kernel_size,
-            padding=kernel_size // 2,
-            bias=False
-        )
-        self.bn = nn.BatchNorm2d(1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-
-        avg_feat    = torch.mean(x, dim=1, keepdim=True)
-        max_feat, _ = torch.max(x, dim=1, keepdim=True)
-
-        feat = torch.cat([avg_feat, max_feat], dim=1)
-        feat = self.conv(feat)
-        feat = self.bn(feat)
-        attention = self.sigmoid(feat)
-        return attention * x
-
-class CBAM(nn.Module):
-    def __init__(self, channels, reduction_rate=16, kernel_size=7):
-        super(CBAM, self).__init__()
-        self.channel_attention = ChannelAttention(channels,
-                                                      reduction_rate)
-        self.spatial_attention = SpatialAttention(kernel_size)
-
-    def forward(self, x):
-        x = self.channel_attention(x)
-        x = self.spatial_attention(x)
-        return x
 
 # DenseNet-B
 class _Bottleneck(nn.Module):
@@ -96,18 +27,13 @@ class _Bottleneck(nn.Module):
         self.use_dropout = use_dropout
         self.dropout = nn.Dropout(p=0.2)
 
-        # CBAM
-        self.cbam1 = CBAM(channels=interChannels, reduction_rate=2, kernel_size=13)
-        self.cbam2 = CBAM(channels=growth_rate, reduction_rate=2, kernel_size=13)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)), inplace=True)
-        out = self.cbam1(out)
         if self.use_dropout:
             out = self.dropout(out)
         out = F.relu(self.bn2(self.conv2(out)), inplace=True)
         # CBAM
-        out = self.cbam2(out)
         if self.use_dropout:
             out = self.dropout(out)
         out = torch.cat((x, out), 1)
@@ -140,16 +66,11 @@ class _Transition(nn.Module):
         super(_Transition, self).__init__()
         self.bn1 = nn.BatchNorm2d(n_out_channels)
         self.conv1 = nn.Conv2d(n_channels, n_out_channels, kernel_size=1, bias=False)
-        # CBAM
-        self.cbam = CBAM(n_out_channels, reduction_rate=2, kernel_size=13)
-
         self.use_dropout = use_dropout
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)), inplace=True)
-        out = self.cbam(out)
-
         if self.use_dropout:
             out = self.dropout(out)
         out = F.avg_pool2d(out, 2, ceil_mode=True)
@@ -173,11 +94,6 @@ class DenseNet(nn.Module):
             1, n_channels, kernel_size=7, padding=3, stride=2, bias=False
         )
         self.norm1 = nn.BatchNorm2d(n_channels)
-
-        self.cbam = CBAM(n_channels, reduction_rate=2, kernel_size=13)
-
-
-
         self.dense1 = self._make_dense(
             n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout
         )
@@ -219,9 +135,6 @@ class DenseNet(nn.Module):
         out = F.relu(out, inplace=True)
         out = F.max_pool2d(out, 2, ceil_mode=True)
         out_mask = out_mask[:, 0::2, 0::2]
-
-        out = self.cbam(out)
-
         out = self.dense1(out)
         out = self.trans1(out)
         out_mask = out_mask[:, 0::2, 0::2]
@@ -240,8 +153,6 @@ class Encoder(pl.LightningModule):
         self.model = DenseNet(growth_rate=growth_rate, num_layers=num_layers)
 
         self.feature_proj = nn.Conv2d(self.model.out_channels, d_model, kernel_size=1)
-        self.cbam = CBAM(d_model, reduction_rate=2, kernel_size=13)
-
         self.pos_enc_2d = ImgPosEnc(d_model, normalize=True)
 
         self.norm = nn.LayerNorm(d_model)
@@ -266,8 +177,6 @@ class Encoder(pl.LightningModule):
         # extract feature
         feature, mask = self.model(img, img_mask)
         feature = self.feature_proj(feature)
-        feature = self.cbam(feature)
-
         # proj
         feature = rearrange(feature, "b d h w -> b h w d")
 
